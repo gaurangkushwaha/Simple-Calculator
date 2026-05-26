d34f4eeb230f636421bb5f6ab556ce6edd2b3cc2
Gaurang Kushwaha <gaurangkushwaha23@gmail.com>
2026-05-26 17:00:58 +0530

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,23 @@
+## Assignment
+## Simple Calculator program
+# Take user input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+# Perform the requested operation
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operation."
+
+print("Result:", result) 
